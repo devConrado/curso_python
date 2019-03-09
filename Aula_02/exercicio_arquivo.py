8fa5114d5547485d7cef3babebfdafa2043b83f3
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-03-09 17:36:09 -0300

    Exercicio_Arquivo

--- Aula_02/exercicio_arquivo.py
+++ Aula_02/exercicio_arquivo.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+def Cadastrar():
+	nome = input('Digite o seu Nome: ')
+	estado = input('Digite o seu Estado: ')
+	cpf = input('Digite o seu CPF: ')
+	with open('arquivo.txt','a') as arquivo:
+		arquivo.write('{}\t {}\t {}\t \n'.format(nome,estado,cpf))
+
+def Buscar():
+	busca = input('Digite o nome que deseja filtrar: ')
+	with open('arquivo.txt') as arquivo:
+		l = []
+		for linha in arquivo:
+			l.append(linha)
+
 
 print('Selecione uma das opções:')
 print('1- Cadastrar Dados')
@@ -6,11 +20,12 @@ print('2- Buscar Dados')
 op = int(input('Digite a opção: '))
 
 if op == 1:
-	def Cadastrar():
-		nome = input('Digite o seu Nome: ')
-		estado = input('Digite o seu Estado: ')
-		cpf = input('Digite o seu CPF: ')
+	Cadastrar()
+elif op == 2:
+	Buscar()
+else:
+	print('Opção não encontrada por favor tente outra opção!')
 
-		
+
 
 
